c8786b75db1404277a9c20ece3a0615148454c3d
Shohibulikhsan <shohibuli89@gmail.com>
2023-01-10 20:04:14 +0700

    Manage Dokter

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO "STATUS" VALUES
+('72b11cec-0080-496a-a3a9-92494007befb', 'Imported lead'),
+('5ce3bbc1-6561-4c71-aa33-382692322b40', 'Not contacted'),
+('d6c3fe4e-e7c9-4961-b7fe-9350242992e9', 'Contacted'),
+('983b41a6-f1b6-4309-87df-5bfd2048392f', 'Customer'),
+('b501cfdc-ea0b-4659-b6d5-0449467a9ca0', 'Closed (lost)');
+INSERT INTO "COMPANY" VALUES
+('48767ba4-a9a2-4c0b-9ce9-cf0c53e8e45e', 'Phillips Van Heusen Corp.'),
+('c58f8dd9-95dc-4d01-836b-4df6b37de527', 'Avaya Inc.'),
+('8cf87de5-574d-44ab-bf7f-25dec35cca73', 'Laboratory Corporation of America Holdings'),
+('2fb669ec-0659-49fb-af9f-e6f71ce8156f', 'AutoZone, Inc.'),
+('8db2356e-c5a4-461d-b231-6e13d4b48757', 'Linens ''n Things Inc.');   
+
+
+
